4364a6453f87ec3e1b8ca3f76561759e5cfab125
Ademola <ademolaogunmokun@yahoo.com>
2022-02-24 13:58:28 +0100

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/index'
   devise_for :users
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
