c6f46da6e9de84e8d1510ebd50b276bac5ff0da7
Ademola <ademolaogunmokun@yahoo.com>
2022-02-25 20:48:21 +0100

    add author

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :posts
+  validates :password, presence: true
+  validates :email, presence: true, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_24_094930) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_25_194024) do
   create_table "posts", force: :cascade do |t|
     t.text "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
